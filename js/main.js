0de794aabb11e66b07a1093c0a57294cb73f47f4
guisposito <guicarlosjoinhas@gmail.com>
2023-06-26 20:43:42 -0300

    Fui longe

--- js/main.js
+++ js/main.js
@@ -2,22 +2,66 @@
 let listaDeItens = [];
 //Coletando dados do formulário
 const form = document.querySelector('[data-form]');
-
+//Pegando Lista para inserção
+const ulItens = document.getElementById('lista-itens');
+//Pega o input do item
+const itensInput = document.getElementsByClassName('item-input');
+//Recebe informações do local Storage
+const listaRecuperada =  localStorage.getItem('listaDeItens', )
 
 form.addEventListener('submit', (ev) => {
     ev.preventDefault();
-    console.log(ev.target.elements['nome'].value);
-
-    listaDeItens.push({
-        nome: ev.target.elements['nome'].value,
-        qtd: ev.target.elements['quantidade'].value
-    });
-
+    
+    salvarItens(ev);
     console.log(listaDeItens);
-    atualizaLocalStorage();
+    mostrarItens();
 });
 
+function salvarItens (evento){
+    const nome = evento.target.elements['nome'].value;
+    const qtd = evento.target.elements['quantidade'].value;
+    
+
+    if(nome != "" || qtd != ""){
+        listaDeItens.push({
+            nome: nome,
+            qtd: qtd
+        });
+    } else {
+        alert('Preencha os campos');
+    }
+}
 
 function atualizaLocalStorage() {
     localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens));
+}
+
+if(listaRecuperada) {
+    //Implementa a lista de json para o novo objeto
+    listaDeItens = JSON.parse(listaRecuperada);
+    mostrarItens();
+} else {
+    listaDeItens = [];
+}
+
+function mostrarItens(){
+    ulItens.innerHTML='';
+
+    listaDeItens.forEach((item, index) => {
+        ulItens.innerHTML += `
+            <li class="item" data-value="${index}">
+                <strong>${item.qtd}</strong>
+                ${item.nome}
+            </li>
+        `
+    })
+
+    atualizaLocalStorage();
+    limpaItens(itensInput);
+}
+
+function limpaItens(inputs) {
+    for(let i  = 0; i < inputs.length; i++){
+        inputs[i].value="";
+    }
 }
