3e1f8c134551698fdbaf0acd3fd1ea827e5a2fa5
guisposito <guicarlosjoinhas@gmail.com>
2023-06-27 20:21:52 -0300

    entendendo melhor o appendchild

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const form = document.getElementById("novoItem");
 
+const lista = document.getElementById("lista-itens");
+
+
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
-    console.log(evento)
+    
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 })
+
+function criaElemento(nome, quantidade) {
+    
+    //cria elemento
+    const novoItem = document.createElement('li')
+    //adiciona classe
+    novoItem.classList.add("item");
+    //Cria elemento do numero
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+    //coloca o numero dentro do elemento de li
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+}   
