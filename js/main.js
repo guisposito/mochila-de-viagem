52c385ffa6d70b8255c1010ade648c690bce8a68
guisposito <guicarlosjoinhas@gmail.com>
2023-06-28 19:31:06 -0300

    Refatorando parcialmente o codigo

--- js/main.js
+++ js/main.js
@@ -3,45 +3,47 @@ const form = document.getElementById("novoItem");
 //coleta lista
 const lista = document.getElementById("lista-itens");
 //pega lista ou cria o objeto vazio
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+const itens = JSON.parse(localStorage.getItem("Itens")) || [];
 
+
+itens.forEach((el) => {
+    criaElemento(el);
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const nome = evento.target.elements['nome'] ;
     const quantidade = evento.target.elements['quantidade'];
 
-    criaElemento(nome.value , quantidade.value);
+    const itemAtual = {
+        "nome" : nome.value,
+        "qtd" : quantidade.value
+    }
+
+    criaElemento(itemAtual);
+    //salvando itens no array
+    itens.push(itemAtual);
+    //setando no localstorage
+    localStorage.setItem("Itens", JSON.stringify(itens));
 
     nome.value = "";
     quantidade.value = "";
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
     //cria elemento
     const novoItem = document.createElement('li')
     //adiciona classe
     novoItem.classList.add("item");
     //Cria elemento do numero
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.qtd;
     //coloca o numero dentro do elemento de li
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
 
     //implementa o item na lista
     lista.appendChild(novoItem);
-
-    const itemAtual = {
-        "nome" : nome,
-        "qtd" : quantidade
-    }
-    //salvando itens no array
-    itens.push(itemAtual);
-
-    //setando no localstorage
-    localStorage.setItem("Itens", JSON.stringify(itens));
-      
 } 
 
 //limpa localstorage
@@ -51,6 +53,3 @@ document.getElementById("limpa").addEventListener('click', () => {
 })
 
 
-itens.forEach((el) => {
-    console.log(el.nome, el.quantidade);
-})
